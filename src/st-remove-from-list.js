d5164b5f34badabea6e7f277b2eddfd67f0dbc8d
AlexShyshko <shish1988@mail.ru>
2021-10-13 23:36:51 +0300

    feat: done singly linked list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  let listTemp = l;
+
+  while (listTemp.next !== null) {
+    if (listTemp.next.value === k) {
+      listTemp.next = listTemp.next.next;
+    } else {
+      listTemp = listTemp.next;
+    }
+  }
+    
+  return l;
 }
